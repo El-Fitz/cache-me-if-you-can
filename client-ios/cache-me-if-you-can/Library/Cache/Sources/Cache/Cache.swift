169b8054fe141873359ebb96a96ebc37bf568f15
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2025-08-20 20:39:44 +0200

    Import FoundationNetworking for Cache package

--- client-ios/cache-me-if-you-can/Library/Cache/Sources/Cache/Cache.swift
+++ client-ios/cache-me-if-you-can/Library/Cache/Sources/Cache/Cache.swift
@@ -2,6 +2,9 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public struct CacheResult {
     public let cacheConfiguration: CacheConfiguration
